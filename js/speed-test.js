3d9d28165020fbac3bfa0aa37c35971efeca725e
ravzzy <swiftwindwalker2@gmail.com>
2025-03-17 20:01:40 +0530

    changing url for mobile

--- js/speed-test.js
+++ js/speed-test.js
@@ -48,7 +48,7 @@ async function startTest() {
   let fileUrl;
 
   if (isMobileDevice()) {
-    fileUrl = `https://ipv6-c008-bom001-bhartiairtel-isp.1.oca.nflxvideo.net/speedtest/range/0-26214399?c=in&n=134450&v=86&e=1742221875&t=AJxhzX4ZMyKO31dWUlFczhjNlc1YXyhDtEylAQ`; // URL for mobile devices
+    fileUrl = `https://speed.cloudflare.com/__down?measId=7795217352823337&bytes=10000000`; // URL for mobile devices
   } else {
     fileUrl = `https://speed.cloudflare.com/__down?measId=4620545399793317&bytes=25000000`; // URL for desktop devices
   }
